fixed network with n outputs and softmax to prepare for categorical distribution

MarioCNNPPO.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class MarioCNNPPO(nn.Module):
 
-    def __init__(self):
+    def __init__(self,n_actions):
         super().__init__()
         self.conv = nn.Sequential(
            nn.Conv2d(4,32,kernel_size=8,stride=4),nn.ReLU(),
@@ -17,7 +17,11 @@ class MarioCNNPPO(nn.Module):
             nn.Linear(3136,512),nn.Tanh() #not sure why 512
         )
         self.value = nn.Linear(512,1)
-        self.dist = nn.Linear(512,2)
+        self.dist = nn.Sequential(
+            nn.Linear(512,n_actions),nn.Softmax()
+        )
+
+
 
 
 

Trainer.py
@@ -1,10 +1,8 @@
 import sys
-import numpy
-print("Python executable:", sys.executable)
-print("NumPy version:", numpy.__version__)
-print("NumPy location:", numpy.__file__)
-
-
+import numpy as np
+import torch.nn as nn
+import torch.optim
+from MarioCNNPPO import MarioCNNPPO
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
@@ -14,15 +12,19 @@ from shimmy import GymV21CompatibilityV0
 # gymnasium — so we build it with the legacy gym API first
 env = gym_super_mario_bros.make("SuperMarioBros-v0")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-#then wrap it so it behaves like a gymnasium env
 
+#then wrap it so it behaves like a gymnasium env
 env = GymV21CompatibilityV0(env=env, render_mode="human")
 
+ppo_model = MarioCNNPPO(env.action_space.shape)
+optimizer = torch.optim.Adam(ppo_model.parameters(),lr=0.01)
 
 def get_action():
     pass
 
-
+#GAE
+def calculate_advantages():
+    pass
 
 
 epochs = 1000
@@ -35,3 +37,4 @@ for i in range(epochs):
         env.render()
 
 env.close()
+